4fcce710ebffdfac288bfdf04b5d50f6fa16bed4
felixschulz385 <felix@zoelch-schulz.de>
2022-10-26 11:30:24 +0200

    Some conceptual changes to the data modules

--- modules/data/data_abstract.py
+++ modules/data/data_abstract.py
@@ -6,3 +6,15 @@ class data_abstract(ABC):
     @abstractmethod
     def __init__(self):
         pass
+    
+    @abstractmethod
+    def push_query(self, spatial_bounds, temporal_bounds, crs):
+        pass
+    
+    @abstractmethod
+    def run_query(self):
+        pass
+    
+    @abstractmethod
+    def export_query(self):
+        pass
