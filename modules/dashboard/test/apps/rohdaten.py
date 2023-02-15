25d2122b8c86d173970561d640b120b6196bd4d1
janbesler <jan_besler@online.de>
2023-02-15 16:21:51 +0100

    texte dashboard

--- modules/dashboard/test/apps/rohdaten.py
+++ modules/dashboard/test/apps/rohdaten.py
@@ -5,7 +5,6 @@ import numpy as np
 from dash import Dash, html, dcc, dash_table
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
-import dash_table
 
 from app import app
 
--- modules/dashboard/test/index.py
+++ modules/dashboard/test/index.py
@@ -1,6 +1,4 @@
 
-import dash_core_components as dcc
-import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 from dash import dcc, html
